748e29420dc51da3e1caa4e17bd54bf3c2fed51c
EdwinGuerreroCortez <116528014+EdwinGuerreroCortez@users.noreply.github.com>
2024-01-18 00:03:00 -0600

    Update funciones.js

--- funciones.js
+++ funciones.js
@@ -1,30 +1,7 @@
-import { division as div } from "./calculadora";
-function suma(v1,v2){
-    return v1+v2;
-}
+const suma = (v1, v2) => v1 + v2;
+const resta = (v1, v2) => v1 - v2;
+const multiplicacion = (v1, v2) => v1 * v2;
+const division = (v1, v2) => (v2 === 0 ? "Error" : v1 / v2);
 
+export	{suma,resta,multiplicacion,division};
 
-(v1,v2)=>{
-    return v1+v2;
-}
-    
-(v1,v2)=> v1+v2;
-
-x=>x*x;
-
-const cuadrado=x=>x*x;
-
-const cuadrado2=(x)=>x*x;
-
-const cuadrado3=(x)=>{
-    return x*x;
-}
-
-
-cuadrado(5);
-
-func(()=>{
-    console.log("hola")
-},x)
-
-div(2,8)
